660513e236a5d6d9bfbe6cffa01d69648b9730aa
GindaChen <32371474+GindaChen@users.noreply.github.com>
2019-04-12 13:21:30 -0500

    New lampy framwork proposaal

---
+++ lampy/src2/lampy.py
@@ -0,0 +1,29 @@
+
+
+class LampyTask:
+    pass
+
+
+class LampyScheduler:
+    pass
+
+
+class LampyOperator:
+    pass
+
+
+# Object Communication Entities
+class LampyObject:
+    pass
+
+
+class LampyClient:
+    pass
+
+
+class LampyServer:
+    pass
+
+
+class LampyWorker:
+    pass
