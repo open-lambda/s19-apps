1aef047e72e1c0670faac26a0a2585d90432bd9f
GindaChen <32371474+GindaChen@users.noreply.github.com>
2019-04-26 02:56:46 -0500

    Clear data

--- lampy/README.md
+++ lampy/README.md
@@ -1,27 +1,6 @@
-# Lampy
+# lampy
 
-Numpy for Open-Lambda.
-
-Demo
-https://docs.google.com/presentation/d/1Szf8G7mzC7mwRbOxSM-Sc_jxLqiJ0RuVDHO5ZffbJEA/edit?usp=sharing
-
-
-## Possible Approach
-
-- **Sparx approach**: We always have the whole information about what to compute. We construct, optimize and dispatch computational diagram.
-- **Just-in-time approach**: We expect changes and flexibility. We do just-in-time call of parallelization. We respect regions of parallelism.
-
-
-
-
-
-
-
-## References
-
-- [Tensorflow: A System for Large-Scale Machine Learning](https://www.tensorflow.org/about/bib)
-- [Tensorflow: Large-Scale Machine Learning on Heterogeneous Distributed Systems](https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/45166.pdf)
-- [Thrust](https://github.com/thrust/thrust/wiki)
-- [Numpy](https://arxiv.org/pdf/1102.1523.pdf)
-- [Julia](https://github.com/JuliaLang/julia/tree/v1.1.0)
-
+## Quick start
+- Create a new cluster
+- Copy this lampy package to `/cluster-path/packages/lampy`
+- From `lampy/testing` copy the registries to `/cluste-path/registry/`
---
+++ lampy/testing/test_core.py
@@ -0,0 +1,109 @@
+import json
+import pickle
+
+from core import lampy as np
+
+# # Client/Server unsingleton identity
+# client = np.Client()
+# print client.lampie
+# print np.coordinator, client
+# assert client == np.coordinator
+#
+# client2 = np.Client()
+# print client.lampie
+# print np.coordinator, client
+# assert client != client2
+#
+#
+# server = np.Server()
+# print server.lampie
+# print server, np.coordinator
+# assert server != client
+
+
+# # Picklability
+# a = np.array([1, 2])
+# print client.lampie
+# b = np.array([1, 2])
+# print client.lampie
+# c = a + b
+# print client.lampie
+# s = client.dumps()
+# print s
+# t = client.loads(s)
+# print client.lampie
+
+
+# # Server Functionality
+# server = np.Server()
+# a = np.array([3.])
+# print "a", a
+# b = np.array([1.5])
+# print "b", b
+# c = a + b
+# print c
+# c = a - b
+# print c
+# c = a * b
+# print c
+# c = a / b
+# print c
+#
+# a = 3
+# print "a", a
+# b = np.array([1.5])
+# print "b", b
+# c = a + b
+# print c
+# c = a - b
+# print c
+# c = a * b
+# print c
+# c = a / b
+# print c
+#
+#
+# a = np.array([3.])
+# print "a", a
+# b = 1.5
+# print "b", b
+# c = a + b
+# print c
+# c = a - b
+# print c
+# c = a * b
+# print c
+# c = a / b
+# print c
+
+# # Delay Definition
+# client = np.Client()
+# print client.delay
+# a = np.array([1])
+# b = np.array([2])
+# c = a + b
+# with np.delay():
+#     d = a - b
+#     print client.delay
+#     print d
+# print d
+
+# File operation
+import h5py
+import numpy
+with h5py.File("tmp.hdf5", "w") as f:
+    a = numpy.random.random(size=(100, 100))
+    b = numpy.random.random(size=(100, 100))
+    f.create_dataset('a', data=a)
+    f.create_dataset('b', data=b)
+
+with h5py.File("tmp.hdf5", "r") as f:
+    a = f.get("a")
+    b = f.get("b")
+    na = np.array(numpy.array(a))
+    nb = np.array(numpy.array(b))
+    with np.delay():
+        nc = na + nb
+        print nc
+    print nc
+# Client Functionality
