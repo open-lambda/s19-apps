4e51a9fd22122b7f75370b95cd7d3ed5b9146243
GindaChen <32371474+GindaChen@users.noreply.github.com>
2019-04-26 02:58:06 -0500

    Update lampy.py

--- lampy/core/lampy.py
+++ lampy/core/lampy.py
@@ -16,3 +16,23 @@ def delay():
         coordinator.delay = old
 
 
+# Global Variable -- should not be access by user eventually
+
+class Pie:
+    def __init__(self, name, value, children_name, func):
+        self.name = name
+        self.value = value
+        self.children_name = children_name or []
+        self.func = func or "add"
+        coordinator.set_pie(name, self)
+
+    def get_value(self):
+        if self.type != "const":
+            global coordinator
+            coordinator.calc_pie(self)
+            pass
+        pass
+
+    def __str__(self):
+        self.get_value()
+        return "<%s [type=%s, name=%s] (%s)>" % (str(self.__class__), self.type, self.name, self.value)
