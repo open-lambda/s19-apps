da34661ea448e8c6d9072251458947b168c43bed
GindaChen <32371474+GindaChen@users.noreply.github.com>
2019-04-26 02:58:21 -0500

    Add operators

--- lampy/core/lampy.py
+++ lampy/core/lampy.py
@@ -46,3 +46,39 @@ class Pie:
     def __str__(self):
         self.get_value()
         return "<%s [type=%s, name=%s] (%s)>" % (str(self.__class__), self.type, self.name, self.value)
+
+    def __repr__(self):
+        return self.__str__()
+
+    @staticmethod
+    def __binaryop__(a, b, func):
+        a = array(a)
+        b = array(b)
+        obj = array(None, ch=[a.name, b.name], func=func)
+        coordinator.calc_pie(obj)
+        return obj
+
+    def __add__(self, other):
+        return Pie.__binaryop__(self, other, "add")
+
+    def __radd__(self, other):
+        return Pie.__binaryop__(other, self, "add")
+
+    def __sub__(self, other):
+        return Pie.__binaryop__(self, other, "sub")
+
+    def __rsub__(self, other):
+        return Pie.__binaryop__(other, self, "sub")
+
+    def __mul__(self, other):
+        return Pie.__binaryop__(self, other, "mul")
+
+    def __rmul__(self, other):
+        return Pie.__binaryop__(other, self, "mul")
+
+    def __div__(self, other):
+        return Pie.__binaryop__(self, other, "div")
+
+    def __rdiv__(self, other):
+        return Pie.__binaryop__(other, self, "div")
+
